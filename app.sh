11aff8ac8c8518d85c1aecccf765371e5520e71b
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-05-19 13:04:21 +0530

    Remove static files

--- app.sh
+++ app.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "install" ]; then
+if [ "$1" == "build" ]; then
+    podman build -t pathproxy .
+elif [ "$1" == "install"]; then
     podman run --name pathproxy --pod pathproxy-pod --replace -d localhost/pathproxy:latest
 elif [ "$1" == "stop" ]; then
     podman stop pathproxy
 elif [ "$1" == "start" ]; then
     podman start pathproxy
 else
-    echo "Usage: $0 {install|stop|start}"
+    echo "Usage: $0 {build|install|stop|start}"
     exit 1
 fi
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
-var path = require('path');
 
 const proxiedUrl = process.env.PROXIED_URL || "http://10.0.0.5/stream.mjpg";
 
 app.get('/stream.mjpg', new MjpegProxy(proxiedUrl).proxyRequest);
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
 
 app.listen(8080);
 console.log('magic is happening');
